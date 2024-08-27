c2b72658cf8f2fcc7865640f598aa9fc6a46df9c
uwi0 <kakapoweb@gmail.com>
2024-08-27 19:37:30 +0700

    Initial Commit

---
+++ Flex To Grid/Flex_To_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flex_To_GridApp.swift
+//  Flex To Grid
+//
+//  Created by dwi prasetyo on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Flex_To_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
